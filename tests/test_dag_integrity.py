a18504fcfe8aacf425a215d241a58ae8e1c8344c
Chas Fiorenza <admin@chasf.altostrat.com>
2024-11-15 18:44:42 +0000

    update dag name

--- tests/test_dag_integrity.py
+++ tests/test_dag_integrity.py
@@ -1,23 +1,24 @@
 import pytest
 import os
 from airflow.models import DagBag
-# from airflow.utils.dag_cycle_tester import test_cycle as dag_test_cycle
 from airflow.utils.dag_cycle_tester import check_cycle
 
 
 @pytest.fixture()
 def dagbag():
     current_path = os.path.dirname(os.path.abspath(__file__))
-    parent_path = os.path.dirname(current_path)  
-    dags_path = os.path.join(parent_path, 'dags')  
+    parent_path = os.path.dirname(current_path)
+    dags_path = os.path.join(parent_path, "dags")
 
     return DagBag(dag_folder=dags_path, include_examples=False)
 
+
 def test_dag_loaded(dagbag):
-    dag = dagbag.get_dag(dag_id="bq_example")
+    dag = dagbag.get_dag(dag_id="bq_ctas")
     assert dagbag.import_errors == {}
     assert dag is not None
     assert len(dag.tasks) == 1
+
 
 def test_dag_cycles(dagbag):
     for dag in dagbag.dags.values():
