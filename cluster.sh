a34844c49a6bb7a5b3238d9d26707aff36baac69
user <admin@chasf.altostrat.com>
2025-04-11 00:45:40 +0000

    Paramaterize DAG

---
+++ cluster.sh
@@ -0,0 +1,13 @@
+
+gcloud composer environments create etl-orchestration \
+--location=us-central1 \
+--image-version=composer-2.11.5-airflow-2.10.2 \
+--environment-size=small \
+--triggerer-count=1 \
+--triggerer-cpu=1 \
+--triggerer-memory=1
+
+gcloud composer environments storage dags import \
+--environment=etl-orchestration \
+--location=us-central1 \
+--source=gs://cf-cloud-composer-dags/dags/serverless_spark.py
--- dags/serverless_spark.py
+++ dags/serverless_spark.py
@@ -2,36 +2,39 @@ import uuid
 from airflow import models
 from airflow.providers.google.cloud.operators.dataproc import (
     DataprocCreateBatchOperator,
-    DataprocListBatchesOperator
+    DataprocListBatchesOperator,
 )
 
 PROJECT_ID = "cf-data-analytics"
 REGION = "us-central1"
 BASE_BATCH_ID = "demo-serverless-batch"
+PYTHON_FILE_LOCATION = "gs://cf-spark-jobs/serverless_demo.py"
+GCS_DESTINATION = "gs://cf-spark-external/wiki_aggregated"
 
 default_args = {
     "project_id": PROJECT_ID,
     "region": REGION,
 }
 with models.DAG(
-        dag_id="dataproc_serverless",
-        default_args=default_args,
-        schedule=None
+    dag_id="dataproc_serverless_trigger", default_args=default_args, schedule=None
 ) as dag:
-    unique_batch_id = BASE_BATCH_ID + "-" + str(uuid.uuid4())
+    unique_batch_id = "demo-serverless-batch-" + str(uuid.uuid4())
 
     create_batch = DataprocCreateBatchOperator(
         task_id="batch_create",
         batch={
-            "spark_batch": {
-                "main_jar_file_uri": "gs://cf-spark-jobs/jars/ServerlessSpark-3.0.0-jar-with-dependencies.jar"
+            "pyspark_batch": {
+                "main_python_file_uri": PYTHON_FILE_LOCATION,
+                "args": ["--gcs_path=" + GCS_DESTINATION],
             },
-            "runtime_config": {
-                "version": "2.2",
-            }
+            "environment_config": {
+                "peripherals_config": {
+                    "spark_history_server_config": {},
+                },
+            },
         },
         batch_id=unique_batch_id,
-        deferrable=True
+        deferrable=True,
     )
 
     list_batches = DataprocListBatchesOperator(
